13d6e79c23444a85760e9709306f54f201768194
Logan Kinzler <logankinzler@gmail.com>
2024-11-01 13:59:02 -0400

    Added the flood fill algorithm and set up for connecting clicks to playing game

---
+++ Assets/Scripts/FloodFillInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloodFillInfo : ScriptableObject
+{
+    public int player;
+    public List<Diamond> previouslySearched;
+    public List<Diamond> surrounding;
+    public bool completelySurrounded;
+}
--- Assets/Scripts/GoGame.cs
+++ Assets/Scripts/GoGame.cs
@@ -1,21 +1,59 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GoGame : MonoBehaviour
 {
+    public FloodFillInfo FloodFill(Diamond startingDiamond) {
+
+        FloodFillInfo FFinfo = ScriptableObject.CreateInstance<FloodFillInfo>();
+        FFinfo.player = startingDiamond.player;
+        FFinfo.previouslySearched = new List<Diamond>();
+        FFinfo.surrounding = GetSurrounding(startingDiamond);
+        FFinfo.completelySurrounded = false;
 
 
+        while (!FFinfo.completelySurrounded) {
+            CullSurrounding(FFinfo.player, FFinfo.previouslySearched, FFinfo.surrounding);
+            
+            for (int ds=FFinfo.surrounding.Count-1; ds>=0; ds--) {
 
-    public Vector3[] FloodFill(Vector3 startingPosition, int[] whiteList) {
-        return new Vector3[0];
+                // WITH AN EMPTY SPACE, THERE'S NOTHING THAT NEEDS TO BE DONE (NO POSSIBLE WAY TO TAKE THE GROUP)
+                if (FFinfo.surrounding[ds].player == 0) return FFinfo;
+                FFinfo.completelySurrounded = true;
+
+                // THERE STILL IS MORE TO EXPAND TO
+                if (FFinfo.surrounding[ds].player == FFinfo.player) {
+                    FFinfo.completelySurrounded = false;
+                    continue;
+                }
+
+                // REMOVE THE NON-SIMILAR PLAYER DIAMONDS SO THEY DON'T GET EXPANDED INTO    
+                FFinfo.surrounding.RemoveAt(ds);
+            }
+
+            // SET UP FOR NEXT ITERATION
+            FFinfo.previouslySearched.AddRange(FFinfo.surrounding);
+
+            List<Diamond> newSurrounding = new List<Diamond>();
+            foreach (Diamond surround in FFinfo.surrounding)
+                newSurrounding.AddRange(GetSurrounding(surround));
+        }
+
+        return FFinfo;
     }
 
-    public Vector3[] GetSurrounding(Vector3 startingPosition, int[] whiteList) {
-        if (GameObject.Find(string.Format("Diamond:({0},{1},{2})",
-            startingPosition.x, startingPosition.y, startingPosition.z))) return new Vector3[4];
+    public void CullSurrounding(int player, List<Diamond> previouslySearched, List<Diamond> surroundingList) {
+        for (int sd=surroundingList.Count-1; sd>=0; sd--) {
+            if (!previouslySearched.Contains(surroundingList[sd])) {surroundingList.RemoveAt(sd); continue;}
+        }
+    }
 
+    public List<Diamond> GetSurrounding(Diamond diamond) {
+        List<Diamond> surroundingDiamonds = new List<Diamond>();
         Vector3[] surrounding = new Vector3[4];
+
+        Vector3 startingPosition = diamond.GetPosition();
         switch (startingPosition.z) {
             case 0: surrounding = new Vector3[4]{
                 new Vector3(startingPosition.x, startingPosition.y, 2),
@@ -38,21 +76,15 @@ public class GoGame : MonoBehaviour
                 new Vector3(startingPosition.x-1, startingPosition.y, 1)
             }; break;
         }
-
-        for (int s=0; s<4; s++) {
-            GameObject surroundingDiamond = GameObject.Find(string.Format("Diamond:({0},{1},{2})",
-                surrounding[s].x, surrounding[s].y, surrounding[s].z));
-
-            if (surroundingDiamond == null) continue;
-            if ( hasElement(whiteList, surroundingDiamond.GetComponent<Diamond>().player) ) continue;
-            surrounding[s] = Vector3.back;
-        }
         
-        return surrounding;
-    }
+        foreach (Vector3 surround in surrounding) {
+            Diamond surroundingDiamond = GameObject.Find(string.Format("Diamond:({0},{1},{2})",
+                surround.x, surround.y, surround.z)).GetComponent<Diamond>();
+            
+            if (surroundingDiamond == null) continue;
+            surroundingDiamonds.Add(surroundingDiamond);
+        }
 
-    private bool hasElement(int[] array, int elem) {
-        foreach (object obj in array) if (obj.Equals(elem)) return true;
-        return false;
+        return surroundingDiamonds;
     }
 }
